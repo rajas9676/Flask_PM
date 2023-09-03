8cfbdd2c784118507e96fff7e7c0146588b1ac7f
Raja sekhar <rajasekharreddynuzvid@gmail.com>
2023-09-03 16:40:43 -0500

    initial commit2

--- app.py
+++ app.py
@@ -36,6 +36,7 @@ setting_cols = ['setting_{}'.format(i + 1) for i in range(3)]
 sensor_cols = ['sensor_{}'.format(i + 1) for i in range(21)]
 feature_cols = setting_cols + sensor_cols
 seq_length = 50
+rul_seq_length = 30
 seq_cols = feature_cols
 
 
@@ -48,11 +49,31 @@ def predict_failure(machine_id, model, df_test):
     return failure_prob
 
 
+def predict_rul(machine_id, model_rul, df_test_rul):
+    machine_df = df_test_rul[df_test_rul.engine_id == machine_id]
+    rul_cols = ['setting_1', 'setting_2', 'sensor_2', 'sensor_3', 'sensor_4', 'sensor_7', 'sensor_8', 'sensor_9',
+                'sensor_11', 'sensor_12', 'sensor_13', 'sensor_14', 'sensor_15', 'sensor_17', 'sensor_20', 'sensor_21']
+    machine_test = gen_sequence(machine_df, rul_seq_length, rul_cols)
+    m_pred = model_rul.predict(machine_test, verbose=0)
+    rul = list(m_pred[-1] * 1)[0]
+    return int(rul)
+
+
 # Define color-coding function
 def get_color_code(probability):
     if probability < 20:
         return 'green'
     elif probability < 50:
+        return 'yellow'
+    else:
+        return 'red'
+
+
+# Define color-coding function
+def get_rul_color(rul):
+    if rul > 30:
+        return 'green'
+    elif 10 <= rul <= 30:
         return 'yellow'
     else:
         return 'red'
@@ -68,10 +89,18 @@ def index():
         model = tf.keras.models.load_model(model_path)
         df_test = pd.read_csv(data_path)
         failure_prob = predict_failure(machine_id, model, df_test)
-        failure_probability = round(failure_prob, 3)
+        failure_probability = round(failure_prob, 2)
         color_code = get_color_code(failure_probability)
-        return render_template('index.html', machine_id=machine_id, probability=failure_probability,
-                               color_code=color_code, grid_data=grid_data)
+        if 'calculate_rul' in request.form:
+            model_rul = tf.keras.models.load_model('models/model_calculate_rul')
+            df_test_rul = pd.read_csv('data/df_rul_test.csv')
+            # Call the predict_rul function to calculate RUL
+            rul_result = predict_rul(machine_id, model_rul, df_test_rul)
+            return render_template('index.html', machine_id=machine_id, probability=failure_probability,
+                                   color_code=color_code, grid_data=grid_data, rul=rul_result)
+        else:
+            return render_template('index.html', machine_id=machine_id, probability=failure_probability,
+                                   color_code=color_code, grid_data=grid_data)
     return render_template('index.html', grid_data=grid_data)
 
 
@@ -96,13 +125,33 @@ def grid():
         failure_prob = predict_failure(machine_id, model, df_test)
         failure_probability = round(failure_prob, 3)
         color_code = get_color_code(failure_probability)
-        grid_data.append({'machine_id': machine_id, 'color_code': color_code, 'failure_probability':failure_probability})
+        grid_data.append(
+            {'machine_id': machine_id, 'color_code': color_code, 'failure_probability': failure_probability})
     green_count = sum(1 for item in grid_data if item['color_code'] == 'green')
     yellow_count = sum(1 for item in grid_data if item['color_code'] == 'yellow')
     red_count = sum(1 for item in grid_data if item['color_code'] == 'red')
 
     return render_template('grid.html', button_clicked=button_clicked, grid_data=grid_data, green_count=green_count,
                            yellow_count=yellow_count, red_count=red_count)
+
+
+@app.route('/rul_grid')
+def rul_grid():
+    grid_data = []
+    model_rul = tf.keras.models.load_model('models/model_calculate_rul')
+    df_test_rul = pd.read_csv('data/df_rul_test.csv')
+
+    for machine_id in range(1, 101):
+        # Call the predict_rul function to calculate RUL
+        rul_result = predict_rul(machine_id, model_rul, df_test_rul)
+        rul_color_code = get_rul_color(rul_result)
+        grid_data.append({'machine_id': machine_id, 'rul_color_code': rul_color_code, 'rul_result': rul_result})
+    rul_green_count = sum(1 for item in grid_data if item['rul_color_code'] == 'green')
+    rul_yellow_count = sum(1 for item in grid_data if item['rul_color_code'] == 'yellow')
+    rul_red_count = sum(1 for item in grid_data if item['rul_color_code'] == 'red')
+
+    return render_template('rul_grid.html', grid_data=grid_data, rul_green_count=rul_green_count,
+                           rul_yellow_count=rul_yellow_count, rul_red_count=rul_red_count)
 
 
 if __name__ == '__main__':
